4ff4a48d76171faabb56e16830edb5f67e56e5d5
Abhishek Rana <53045241+abhishekranaji@users.noreply.github.com>
2019-10-21 19:53:59 +0530

    Add files via upload

---
+++ Deep_sleep_Reed_Interupt/Deep_sleep_Reed_Interupt.ino
@@ -0,0 +1,46 @@
+/**
+ * Author:Ab Kurk
+ * version: 1.0
+ * date: 24/01/2018
+ * Description: 
+ * This sketch is part of the beginners guide to putting your Arduino to sleep
+ * tutorial. It is to demonstrate how to put your arduino into deep sleep and
+ * how to wake it up.
+ * Link To Tutorial http://www.thearduinomakerman.info/blog/2018/1/24/guide-to-arduino-sleep-mode
+ */
+
+#include <avr/sleep.h>//this AVR library contains the methods that controls the sleep modes
+#define interruptPin 3 //Pin we are going to use to wake up the Arduino
+
+
+void setup() {
+  Serial.begin(115200);//Start Serial Comunication
+  digitalWrite(3, LOW);
+  Serial.println(digitalRead(3));
+  digitalWrite(13, LOW);
+  pinMode(7,OUTPUT);//We use the led on pin 13 to indecate when Arduino is A sleep
+  pinMode(interruptPin,INPUT_PULLUP);//Set pin d2 to input using the buildin pullup resistor
+  digitalWrite(7,HIGH);//turning LED on
+}
+
+void loop() {
+ delay(5000);//wait 5 seconds before going to sleep
+ Going_To_Sleep();
+}
+
+void Going_To_Sleep(){
+    sleep_enable();//Enabling sleep mode
+    attachInterrupt(digitalPinToInterrupt(3), wakeUp, CHANGE);//attaching a interrupt to pin d2
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);//Setting the sleep mode, in our case full sleep
+    digitalWrite(7,LOW);//turning LED off
+    delay(1000); //wait a second to allow the led to be turned off before going to sleep
+    sleep_cpu();//activating sleep mode
+    Serial.println("just woke up!");//next line of code executed after the interrupt 
+    digitalWrite(7,HIGH);//turning LED on
+  }
+
+void wakeUp(){
+  Serial.println("Interrrupt Fired");//Print message to serial monitor
+   sleep_disable();//Disable sleep mode
+//  detachInterrupt(0); //Removes the interrupt from pin 2;
+}
